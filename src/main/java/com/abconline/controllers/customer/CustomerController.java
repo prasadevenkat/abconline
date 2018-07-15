f8d8b6c0150f4f1c87db59616b9c31470adb247c
etimbukudofia <euedofia@gmail.com>
2018-07-15 20:12:37 +0100

    added delete capability for customer

--- src/main/java/com/abconline/controllers/customer/CustomerController.java
+++ src/main/java/com/abconline/controllers/customer/CustomerController.java
@@ -1,11 +1,14 @@
 package com.abconline.controllers.customer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abconline.models.customer.Customer;
+import com.abconline.repositories.basket.BasketRepository;
 import com.abconline.repositories.customer.CustomerRepository;
+import com.abconline.repositories.order.OrdersRepository;
+
+import static com.abconline.utils.AbcOnlineStrings.RESPONSE_MESSAGE_KEY;
+import static com.abconline.utils.AbcOnlineStrings.STATUS_KEY;
+import static com.abconline.utils.AbcOnlineStrings.SUCCESS_KEY;
 
 @RestController
 @RequestMapping(value = "/customers")
@@ -22,6 +31,12 @@ public class CustomerController {
 
   @Autowired
   private final CustomerRepository customerRepository;
+
+  @Autowired
+  private BasketRepository basketRepository;
+
+  @Autowired
+  private OrdersRepository ordersRepository;
 
   public CustomerController(CustomerRepository customerRepository) {
     this.customerRepository = customerRepository;
@@ -56,5 +71,28 @@ public class CustomerController {
     }
 
     return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  }
+
+  @DeleteMapping(value = "/{customerId}")
+  public ResponseEntity<?> deleteCustomer(@PathVariable("customerId") long customerId) {
+    if (customerRepository.existsById(customerId)) {
+      //delete customers basket
+      basketRepository.deleteByCustomerId(customerId);
+
+      //delete customers orders
+      ordersRepository.deleteByCustomerId(customerId);
+
+      //now delete customer
+      customerRepository.deleteById(customerId);
+
+      Map<String, String> response = new HashMap<>();
+      response.put(STATUS_KEY, SUCCESS_KEY);
+      response.put(RESPONSE_MESSAGE_KEY, String.format("Successfully deleted customer with id %1$s",
+          customerId));
+
+      return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
   }
 }
