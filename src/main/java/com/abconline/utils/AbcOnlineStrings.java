6ede2eae69baa997c4ecc50603fba9726a314aed
etimbukudofia <euedofia@gmail.com>
2018-07-15 17:58:38 +0100

    commonly used string values

---
+++ src/main/java/com/abconline/utils/AbcOnlineStrings.java
@@ -0,0 +1,19 @@
+package com.abconline.utils;
+
+/**
+ * contains strings used repeatedly in abc online
+ *
+ * @author etimbukudofia
+ */
+public final class AbcOnlineStrings {
+  // rest resource strings
+  public static final String ORDERS_KEY = "orders";
+  public static final String BASKETS_KEY = "baskets";
+  public static final String ITEMS_KEY = "items";
+  public static final String CUSTOMERS_KEY = "customers";
+
+  // generic strings
+  public static final String RESPONSE_MESSAGE_KEY = "message";
+  public static final String STATUS_KEY = "status";
+  public static final String SUCCESS_KEY = "success";
+}
