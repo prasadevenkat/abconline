89ff47ee6fc1b6111ffb1befdadd15444d9734bc
etimbukudofia <euedofia@gmail.com>
2018-07-15 19:57:30 +0100

    updated repositories

---
+++ src/main/java/com/abconline/repositories/basket/BasketRepository.java
@@ -0,0 +1,9 @@
+package com.abconline.repositories.basket;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.abconline.models.basket.Basket;
+
+public interface BasketRepository extends JpaRepository<Basket, Long> {
+
+}
