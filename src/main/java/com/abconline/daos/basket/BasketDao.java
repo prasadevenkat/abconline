fb4f1cde06cf2779aaac914060ff1bf8c7b6f906
etimbuk <etimbuk@feefo.com>
2018-07-31 13:55:09 +0100

    removed unused method

--- src/main/java/com/abconline/daos/basket/BasketDao.java
+++ src/main/java/com/abconline/daos/basket/BasketDao.java
@@ -5,7 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.abconline.models.basket.Basket;
 
 public interface BasketDao extends JpaRepository<Basket, Long> {
-
-  void deleteByCustomerId(Long customer);
-
 }
