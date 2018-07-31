9b2357064760e67adc75ffcbac1b49686af79d84
etimbuk <etimbuk@feefo.com>
2018-07-31 13:56:48 +0100

    updated annotation mappings to improve entity relationships

--- src/main/java/com/abconline/models/basket/Basket.java
+++ src/main/java/com/abconline/models/basket/Basket.java
@@ -1,19 +1,28 @@
 package com.abconline.models.basket;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.springframework.util.CollectionUtils;
 
 import com.abconline.models.customer.Customer;
 import com.abconline.models.order.OrderItem;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
@@ -44,7 +53,7 @@ public class Basket {
 
   @JsonIgnore
   @OneToOne
-  private Customer customer;
+  private Customer customerBasket;
 
   public Basket() {}
 
@@ -56,7 +65,7 @@ public class Basket {
 
   public Basket(LocalDate createdAt, LocalDate updatedAt, List<OrderItem> orderItems, Customer customer) {
     this(createdAt, updatedAt, orderItems);
-    this.customer = customer;
+    this.customerBasket = customer;
   }
 
   public Long getId() {
@@ -71,12 +80,26 @@ public class Basket {
     return updatedAt;
   }
 
+  public void setUpdatedAt(LocalDate updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
   public List<OrderItem> getOrderItems() {
     return orderItems;
   }
 
-  public Customer getCustomer() {
-    return customer;
+  public Customer getCustomerBasket() {
+    return customerBasket;
+  }
+
+  public void addOrderItem(OrderItem item) {
+    if (CollectionUtils.isEmpty(this.orderItems)) {
+      this.orderItems = new ArrayList<>();
+    }
+
+    if (Objects.nonNull(item)) {
+      this.orderItems.add(item);
+    }
   }
 
   @Override
@@ -88,12 +111,12 @@ public class Basket {
             Objects.equals(createdAt, basket.createdAt) &&
             Objects.equals(updatedAt, basket.updatedAt) &&
             Objects.equals(orderItems, basket.orderItems) &&
-            Objects.equals(customer, basket.customer);
+            Objects.equals(customerBasket, basket.customerBasket);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, createdAt, updatedAt, orderItems, customer);
+    return Objects.hash(id, createdAt, updatedAt, orderItems, customerBasket);
   }
 
   @Override
@@ -103,7 +126,7 @@ public class Basket {
             .append("createdAt", createdAt)
             .append("updatedAt", updatedAt)
             .append("orderItems", orderItems)
-            .append("customer", customer)
+            .append("customerBasket", customerBasket)
             .toString();
   }
 }
